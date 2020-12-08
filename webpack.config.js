3193fbcc7ef143a7a9498abe2498d28d9e5d7792
Maris Mihai-Bogdan <maris.mihaibogdan@gmail.com>
2020-12-08 02:00:53 +0200

    Added babel + typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [new HtmlWebpackPlugin(option = {template: './src/index.html'})]
